5882c444e5b589ecd8a9bcbb4b7c488f3b4217f2
Wisnuchandra <2310511026@mahasiswa.upnvj.ac.id>
2025-05-14 21:43:10 +0700

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,1 +1,16 @@
+import json
+import os
 
+FILENAME = "tasks.json"
+
+def load_tasks():
+    if not os.path.exists(FILENAME):
+        return []
+    with open(FILENAME, "r") as f:
+        return json.load(f)
+
+def save_tasks(tasks):
+    with open(FILENAME, "w") as f:
+        json.dump(tasks, f)
+    print("Data tugas berhasil disimpan.")
+  
