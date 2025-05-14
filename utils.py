90450e91a091b840b8c3dc081b01457f2817d1d4
akmaaltaufiq <taufikakmal53@gmail.com>
2025-05-14 21:41:38 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,10 @@
+def print_menu():
+    print("\n=== To-Do List CLI ===")
+    print("1. Tambah Tugas")
+    print("2. Hapus Tugas")
+    print("3. Lihat Daftar Tugas")
+    print("4. Simpan dan Keluar")
+
+def get_user_choice():
+    return input("Pilih opsi (1-4): ")
 
